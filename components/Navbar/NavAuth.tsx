895b530172d58e21abe3190577a22590f3e71dd9
Your Name <you@example.com>
2023-11-03 15:48:16 +0200

    R

--- components/Navbar/NavAuth.tsx
+++ components/Navbar/NavAuth.tsx
@@ -99,7 +99,7 @@ const NavAuth: FC<NavAuthProps> = ({ }) => {
 
           <Link
             className="bg-gradient-44 py-2 px-4 text-white mx-2 border border-white rounded-3xl"
-            href={`/profile/self`}
+            href={'/profile/self'}
           >
             Profile
           </Link>
