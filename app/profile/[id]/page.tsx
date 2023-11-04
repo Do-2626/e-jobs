ee42dbe810ce80d9bea91dc865b66767e5b56160
Your Name <you@example.com>
2023-11-04 10:21:56 +0200

    R

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -33,26 +33,26 @@ const ProfileDetails: FC<profileProps> = ({ params }) => {
   }, [params.id]);
   console.log(user)
 
-  {/* ال لايك */ }
-  let count = false;
-  const like = (e: any) => {
-    const likeButtonStyle = window.document.querySelector("#likeButton")?.classList
-    count ? count = !count : count = true
-    if (count) {
-      likeButtonStyle?.remove("hover:bg-[#e6e6eb]", "hover:text-[#fff]")
-      likeButtonStyle?.add("bg-[#0071c5]", "text-gray-100")
-    } else {
-      likeButtonStyle?.remove("bg-[#0071c5]", "text-gray-100")
-      likeButtonStyle?.add("hover:bg-[#e6e6eb]")
-    }
-  }
+  // {/* ال لايك */ }
+  // let count = false;
+  // const like = (e: any) => {
+  //   const likeButtonStyle = window.document.querySelector("#likeButton")?.classList
+  //   count ? count = !count : count = true
+  //   if (count) {
+  //     likeButtonStyle?.remove("hover:bg-[#e6e6eb]", "hover:text-[#fff]")
+  //     likeButtonStyle?.add("bg-[#0071c5]", "text-gray-100")
+  //   } else {
+  //     likeButtonStyle?.remove("bg-[#0071c5]", "text-gray-100")
+  //     likeButtonStyle?.add("hover:bg-[#e6e6eb]")
+  //   }
+  // }
 
 
-  {/* عدد المشاهدات */ }
-  const show = (e: any) => {
-    e.target.innerText = (parseInt(e.target.innerText) + 1)
-  }
-  setTimeout((() => { console.log(123) }), 5000)
+  // {/* عدد المشاهدات */ }
+  // const show = (e: any) => {
+  //   e.target.innerText = (parseInt(e.target.innerText) + 1)
+  // }
+  // setTimeout((() => { console.log(123) }), 5000)
 
 
   return (
@@ -61,7 +61,7 @@ const ProfileDetails: FC<profileProps> = ({ params }) => {
         {/* زر ال لايك */}
         <Button
           id="likeButton"
-          onClick={like}
+          // onClick={like}
           disabled={params.id == "self"}
           variant={"outline"}
           className="mr-2 text-lg hover:bg-[#e6e6eb]">
@@ -74,7 +74,11 @@ const ProfileDetails: FC<profileProps> = ({ params }) => {
         <span className="show gap-3">
           <span><BiShowAlt /></span>
           <span>|</span>
-          <span onClick={show} >25</span>
+          <span
+          // onClick={show}
+          >
+            25
+          </span>
         </span>
 
         <Button
@@ -85,205 +89,207 @@ const ProfileDetails: FC<profileProps> = ({ params }) => {
           <Link href={"/profile"}>Update Profile</Link>
         </Button>
       </div>
-      {user && (
-        <div className=" overflow-hidden  ">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-            <div className="p-2 border shadow-md">
-              <div className="flex justify-center p-2">
-                <Image
-                  src={'/' + user.role + '-icon.png'}
-                  width={175}
-                  height={175}
-                  alt="Eng-Icon"
-                  className="rounded-full"
-                />
-              </div>
-              <div className="p-2 space-y-2 ">
-                <TextWrapper
-                  title="Full Name"
-                  value={user.fullName}
-                  parentClassName="  "
-                />
+      {
+        user && (
+          <div className=" overflow-hidden  ">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              <div className="p-2 border shadow-md">
+                <div className="flex justify-center p-2">
+                  <Image
+                    src={'/' + user.role + '-icon.png'}
+                    width={175}
+                    height={175}
+                    alt="Eng-Icon"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="p-2 space-y-2 ">
+                  <TextWrapper
+                    title="Full Name"
+                    value={user.fullName}
+                    parentClassName="  "
+                  />
 
-                {/* <TextWrapper
+                  {/* <TextWrapper
                   title="Email"
                   value={user.email}
                   parentClassName="  "
                 /> */}
-                <TextWrapper
-                  title="Gender"
-                  value={
-                    user.gender.charAt(0).toUpperCase() + user.gender.slice(1)
-                  }
-                  parentClassName="  "
-                />
-                <TextWrapper
-                  title="Mobile Number"
-                  value={user.mobile}
-                  parentClassName="  "
-                />
-                <TextWrapper
-                  title="Date Of Birth"
-                  value={user.dateOfBirth.toString().split("T")[0]}
-                  parentClassName="  "
-                />
-                <TextWrapper
-                  title="Role"
-                  value={user.role}
-                  parentClassName="  "
-                />
-              </div>
-            </div>
-            {!user.resume?.static && (
-              <>
-                <div className="p-2 border shadow-md ">
-                  <div className="p-2 space-y-2">
-                    <TextWrapper
-                      title="Country"
-                      value={user.resume.address.country}
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="City"
-                      value={user.resume.address.city}
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="University"
-                      value={user.resume?.education?.university}
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="College"
-                      value={user.resume?.education?.college}
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Field Of Study"
-                      value={user.resume?.education?.major}
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Graduation Year"
-                      value={user.resume?.education?.year.toString()}
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Grade"
-                      value={
-                        user.resume?.education?.grade.toFixed(2).toString() +
-                        "%"
-                      }
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Employee Status"
-                      value={
-                        user.resume?.employeeStatus.charAt(0).toUpperCase() +
-                        user.resume?.employeeStatus.slice(1)
-                      }
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Available  on"
-                      value={
-                        user.resume?.dateAvailable?.toString().split("T")[0] ||
-                        "No Info"
-                      }
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Applied For"
-                      value={
-                        user.resume?.positionAppliedFor
-                          .charAt(0)
-                          .toUpperCase() +
-                        user.resume?.positionAppliedFor.slice(1)
-                      }
-                      parentClassName="  "
-                    />
-                    <TextWrapper
-                      title="Job Type"
-                      value={
-                        user.resume?.jobType.charAt(0).toUpperCase() +
-                        user.resume?.jobType.slice(1)
-                      }
-                      parentClassName="  "
-                    />
-                  </div>
+                  <TextWrapper
+                    title="Gender"
+                    value={
+                      user.gender.charAt(0).toUpperCase() + user.gender.slice(1)
+                    }
+                    parentClassName="  "
+                  />
+                  <TextWrapper
+                    title="Mobile Number"
+                    value={user.mobile}
+                    parentClassName="  "
+                  />
+                  <TextWrapper
+                    title="Date Of Birth"
+                    value={user.dateOfBirth.toString().split("T")[0]}
+                    parentClassName="  "
+                  />
+                  <TextWrapper
+                    title="Role"
+                    value={user.role}
+                    parentClassName="  "
+                  />
                 </div>
-                <div className="p-2 border shadow-md">
-                  <div className=" p-2 space-y-2">
-                    <div className="p-2">
-                      <h3 className=" text-center ">Work Experience</h3>
-                      <div className="">
-                        {user.resume?.workExperience?.map((item, index) => (
-                          <div className="p-2 border shadow my-2 " key={index}>
-                            <TextWrapper
-                              title="Company Name"
-                              value={item?.company}
-                              parentClassName="  "
-                            />
-                            <TextWrapper
-                              title="Position"
-                              value={item.position}
-                              parentClassName="  "
-                            />
-                            <TextWrapper
-                              title="Start Date"
-                              value={item?.from.toString().split("T")[0]}
-                              parentClassName="  "
-                            />
-                            <TextWrapper
-                              title="End Date"
-                              value={item?.to.toString().split("T")[0]}
-                              parentClassName="  "
-                            />
+              </div>
+              {!user.resume?.static && (
+                <>
+                  <div className="p-2 border shadow-md ">
+                    <div className="p-2 space-y-2">
+                      <TextWrapper
+                        title="Country"
+                        value={user.resume.address.country}
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="City"
+                        value={user.resume.address.city}
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="University"
+                        value={user.resume?.education?.university}
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="College"
+                        value={user.resume?.education?.college}
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Field Of Study"
+                        value={user.resume?.education?.major}
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Graduation Year"
+                        value={user.resume?.education?.year.toString()}
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Grade"
+                        value={
+                          user.resume?.education?.grade.toFixed(2).toString() +
+                          "%"
+                        }
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Employee Status"
+                        value={
+                          user.resume?.employeeStatus.charAt(0).toUpperCase() +
+                          user.resume?.employeeStatus.slice(1)
+                        }
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Available  on"
+                        value={
+                          user.resume?.dateAvailable?.toString().split("T")[0] ||
+                          "No Info"
+                        }
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Applied For"
+                        value={
+                          user.resume?.positionAppliedFor
+                            .charAt(0)
+                            .toUpperCase() +
+                          user.resume?.positionAppliedFor.slice(1)
+                        }
+                        parentClassName="  "
+                      />
+                      <TextWrapper
+                        title="Job Type"
+                        value={
+                          user.resume?.jobType.charAt(0).toUpperCase() +
+                          user.resume?.jobType.slice(1)
+                        }
+                        parentClassName="  "
+                      />
+                    </div>
+                  </div>
+                  <div className="p-2 border shadow-md">
+                    <div className=" p-2 space-y-2">
+                      <div className="p-2">
+                        <h3 className=" text-center ">Work Experience</h3>
+                        <div className="">
+                          {user.resume?.workExperience?.map((item, index) => (
+                            <div className="p-2 border shadow my-2 " key={index}>
+                              <TextWrapper
+                                title="Company Name"
+                                value={item?.company}
+                                parentClassName="  "
+                              />
+                              <TextWrapper
+                                title="Position"
+                                value={item.position}
+                                parentClassName="  "
+                              />
+                              <TextWrapper
+                                title="Start Date"
+                                value={item?.from.toString().split("T")[0]}
+                                parentClassName="  "
+                              />
+                              <TextWrapper
+                                title="End Date"
+                                value={item?.to.toString().split("T")[0]}
+                                parentClassName="  "
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <div>
+                        <h3 className=" text-center ">Resume Link</h3>
+                        <a
+                          href={user.resume?.resumeLink}
+                          className="text-amber-900 font-semibold"
+                          target="_blank"
+                        >
+                          {user.resume?.resumeLink}
+                        </a>
+                      </div>
+                      <div>
+                        <h3 className=" text-center ">License Links</h3>
+                        {user.resume?.license?.map((item, index) => (
+                          <div className="" key={index}>
+                            <a
+                              href={item}
+                              className="text-amber-900 font-semibold"
+                              target="_blank"
+                            >
+                              {item}
+                            </a>
                           </div>
                         ))}
                       </div>
                     </div>
-                    <div>
-                      <h3 className=" text-center ">Resume Link</h3>
-                      <a
-                        href={user.resume?.resumeLink}
-                        className="text-amber-900 font-semibold"
-                        target="_blank"
-                      >
-                        {user.resume?.resumeLink}
-                      </a>
-                    </div>
-                    <div>
-                      <h3 className=" text-center ">License Links</h3>
-                      {user.resume?.license?.map((item, index) => (
-                        <div className="" key={index}>
-                          <a
-                            href={item}
-                            className="text-amber-900 font-semibold"
-                            target="_blank"
-                          >
-                            {item}
-                          </a>
-                        </div>
-                      ))}
-                    </div>
                   </div>
-                </div>
-              </>
+                </>
+              )}
+            </div>
+            {user.resume?.resumeLink && (
+              <div className="container p-2">
+                <h2 className="text-center  text-2xl">Resume</h2>
+                <iframe
+                  src={user.resume?.resumeLink.replace("view", "preview")}
+                  className="w-full  h-[100vh] "
+                ></iframe>
+              </div>
             )}
           </div>
-          {user.resume?.resumeLink && (
-            <div className="container p-2">
-              <h2 className="text-center  text-2xl">Resume</h2>
-              <iframe
-                src={user.resume?.resumeLink.replace("view", "preview")}
-                className="w-full  h-[100vh] "
-              ></iframe>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
+        )
+      }
+    </div >
   );
 };
 
